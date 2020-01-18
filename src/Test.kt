edf716dd79272f0c68d95ae630b7b9c6adfd4b75
Arbaz Pirwani <arbaz@oraan.com>
2020-01-18 17:15:10 +0500

    Initial commit

---
+++ src/Test.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+
+    val name = "Arbaz"
+
+    var course: String = "Android"
+
+    var age = 23
+
+    val year: Int = 2020
+
+    var percentage: Float = 96.4F
+
+    val salary: Double = 50_000.00
+
+    println(salary)
+
+}
+
